088a28b560bcfbf35f47e5bb9c7c68e1849b7a9a
Marazorq <shuratov2001@mail.ru>
2021-05-05 01:45:54 +0300

    created form

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([])
+
   return (
     <div className="App">
-      s
+        <form>
+            <input type="text"/>
+            <input type="number"/>
+            <div className="btns">
+                <button>Created</button>
+                <button>Get</button>
+            </div>
+            <div>
+                {users.map(user =>
+                  <div className="user">{user.id}. {user.username} {user.age}</div>  
+                )}
+            </div>
+        </form>
     </div>
   );
 }
